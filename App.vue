7b4e6751017439a505d7f195d7f7a4bc338fc90a
sunshinebob <360444887@qq.com>
2019-10-25 18:03:39 +0800

    '初始化项目'

---
+++ App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		// app应用开启时候
+		onLaunch: function() {
+			// 更新检测
+			// 网络监听 
+			// 初始化数据加载
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		// app隐藏的时候
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 引入官方公共css */
+	@import "./common/uni.css";
+	/* 引入基础图标库 */
+	@import "./common/icon.css";
+	/* 引入动画看 */
+	@import "./common/animate.css"
+</style>
---
+++ pages/demo/demo.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="view-box animated" hover-class="fadeIn" hover-stay-time="800"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.view-box {
+		margin: 30px auto;
+		width: 100rpx;
+		height: 100rpx;
+		background-color: #007AFF;
+	}
+</style>
